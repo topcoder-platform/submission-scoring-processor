6f87c37fc0411e3a67af5e29280db789fcb941f6
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-02-25 16:19:09 +0530

    Update build.sh
    [circlci skip]

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 ENV=$1
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
+#AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+#AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+#AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
 
 # Builds Docker image of the app.
 TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
