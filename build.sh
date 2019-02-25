d9510e603e62cac761b4cf28993718b1da34e520
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-02-25 16:49:20 +0530

    Update build.sh
    [skip ci]

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -eo pipefail
-ENV=$1
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
+#ENV=$1
+#AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+#AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+#AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
 
 # Builds Docker image of the app.
-TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
-sed -i='' "s|submission-scoring-processor:latest|$TAG|" docker/docker-compose.yml
+#TAG="submission-scoring-processor:latest"
+#TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
+#sed -i='' "s|submission-scoring-processor:latest|$TAG|" docker/docker-compose.yml
 echo "" > docker/api.env
 docker-compose -f docker/docker-compose.yml build submission-scoring-processor
 docker images
